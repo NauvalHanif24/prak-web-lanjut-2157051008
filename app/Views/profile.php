19cd71b144f8b4cd9ae964919337c4e01ec93ea5
Nauval Hanif <nauvalhanif8@gmail.com>
2023-09-14 18:32:02 +0700

    tugas weblanjut 2

---
+++ app/Views/profile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            width:100%;
+            height:100vh;
+            display : flex;
+            align-items: center;
+            justify-content: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="profile">
+        <img src="<?=base_url('image.jpg');?>" style="width : 200px;">
+        <h1><?=$nama?></h1>
+        <h1><?=$kelas?></h1>
+        <h1><?=$npm?></h1>
+    </div>
+</body>
+</html>
