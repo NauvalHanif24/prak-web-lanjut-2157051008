0ccef612906c0f3950c2fad9cff61b736768ef11
Nauval Hanif <nauvalhanif8@gmail.com>
2023-10-28 00:32:10 +0700

    update-delete

--- app/Views/list_user.php
+++ app/Views/list_user.php
@@ -25,8 +25,12 @@
             <td><?= $user['nama_kelas'] ?></td>
             <td>
               <a class="btn btn-primary" href="<?= base_url('user/' . $user['id'])?>">Detail</a>
-              <a class="btn btn-warning" href="">Edit</a>
-              <a class="btn btn-danger" href="">Hapus</a>
+              <a class="btn btn-warning" href="<?= base_url('/user/' . $user['id'] . '/edit') ?>">Edit</a>
+              <form action="<?= base_url('/user/' .  $user['id']) ?>" method="post">
+              <input type="hidden" name="_method" value="DELETE">
+              <?= csrf_field() ?>
+              <button type="submit" class="btn btn-danger" >Delete</button>
+              </form>
 
             </td>
           </tr>
